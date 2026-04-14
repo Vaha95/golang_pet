04fb75349868de11d466ca7438d4c0243cbf44d9
Вахтанг Шенгелия <v.shengeliia@mts-link.ru>
2026-04-14 18:58:00 +0300

    iter15:fix

--- internal/config/db/migrations/260414172610_deleted_uniq_idx.down.sql
+++ internal/config/db/migrations/260414172610_deleted_uniq_idx.down.sql
@@ -1,3 +1,5 @@
 CREATE UNIQUE INDEX IF NOT EXISTS unique_urls_idx ON url_short (url);
 
 DROP INDEX IF EXISTS unique_urls_not_deleted_idx;
+
+DROP INDEX IF EXISTS short_by_user_idx;
--- internal/config/db/migrations/260414172610_deleted_uniq_idx.up.sql
+++ internal/config/db/migrations/260414172610_deleted_uniq_idx.up.sql
@@ -2,4 +2,8 @@ CREATE UNIQUE INDEX unique_urls_not_deleted_idx IF NOT EXISTS
 ON url_short (url) 
 WHERE (deleted_at IS NULL);
 
+CREATE INDEX IF NOT EXISTS short_by_user_idx
+ON url_short (short, created_by_user)
+WHERE (deleted_at IS NULL);
+
 DROP INDEX IF EXISTS unique_urls_idx;
