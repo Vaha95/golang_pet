0d26bbbd56413fe67a0fd89984f2cab520ed7d0a
Вахтанг Шенгелия <v.shengeliia@mts-link.ru>
2026-04-14 17:20:56 +0300

    iter15:fix

--- internal/config/db/migrations/260412162530_deleted_at.up.sql
+++ internal/config/db/migrations/260412162530_deleted_at.up.sql
@@ -1,1 +1,7 @@
 ALTER TABLE url_short ADD COLUMN deleted_at TIMESTAMP NULL;
+
+CREATE UNIQUE INDEX unique_urls_not_deleted_idx IF NOT EXISTS
+ON url_short (url) 
+WHERE (deleted_at IS NULL);
+
+DROP INDEX IF EXISTS unique_urls_idx;
